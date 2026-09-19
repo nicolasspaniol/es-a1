726832fd7c121c24eee187e3d3692d7a230989ce
nmspaniol <nicolasmspaniol@gmail.com>
2026-09-19 18:42:34 -0300

    fixes function naming and removed function call from test_order

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,8 +1,8 @@
-from src.es_a1.order import OrderBuilder, Product
+from es_a1.order import OrderBuilder, Product
 
 
-def TestOrder():
-
+# TODO: mudar nome da funcao
+def test_order_():
     pedido = (OrderBuilder()
               .com_cliente("Maria Silva")
               .adicionar_produto(Product("Teclado", 100.0))
@@ -11,7 +11,3 @@ def TestOrder():
     
     assert pedido.cliente == "Maria Silva", "Error cliente"
     assert pedido.endereco == "Rua Central 456", "Error endereco"
-
-
-# Ejecutamos la función de pruebas
-TestOrder()
